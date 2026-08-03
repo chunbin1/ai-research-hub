75368fa3b9b793724624201453a340b2351d40b2
chunbin1 <545360100@qq.com>
2026-08-03 12:44:06 +0800

    build(client): 接入 antd 6,用 CSS layer 与 Tailwind 共存

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { StyleProvider } from '@ant-design/cssinjs'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
 import './index.css'
 import App from './App'
 
@@ -9,8 +12,14 @@ if (!rootEl) throw new Error('Root element #root not found')
 
 createRoot(rootEl).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* layer:把 antd 样式注入 @layer antd,配合 index.css 首行的层序,
+        让 preflight 盖不住 antd、而 Tailwind utility 仍能盖过 antd */}
+    <StyleProvider layer>
+      <ConfigProvider locale={zhCN}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConfigProvider>
+    </StyleProvider>
   </StrictMode>,
 )
